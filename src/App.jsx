fddece69a496d470f25933308371dfa140703102
chibuzo <uchibuzo1@gmail.com>
2024-10-04 01:34:12 +0100

    pages addd

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Main from "./Components/Main";
-// import Home from "./Pages/Home";
+import Home from "./Pages/Home";
+import Works from "./Pages/Works";
+import Contact from "./Pages/Contact";
+import About from "./Pages/About";
+import Freelancers from "./Pages/Freelancers";
+import Employers from "./Pages/Employers";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Main />}>
-          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/freelancers" element={<Freelancers />} />
+          <Route path="/works" element={<Works />} />
+
+          <Route path="/employers" element={<Employers />} />
         </Route>
       </Routes>
     </>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 import Logo from "../assets/Frame 12@2x.png";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <main className="flex justify-between">
+    <main className="flex justify-around">
       <div>
-        <img src={Logo} alt="" className="" />
+        <img src={Logo} alt="" className="w-[10vw]" />
       </div>
-      <div className="flex">
-        <p>Home</p>
-        <p>How it works</p>
-        <p>For Freelancers</p>
-        <p>For Employers</p>
-        <p>About Us</p>
-        <p>Contact Us</p>
+      <div className="flex justify-between w-[60vw] items-center text-[#A19D9D] text-[18px] border-2 border-black">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/works">How it works</NavLink>
+        <NavLink to="/freelancers">For Freelancers</NavLink>
+        <NavLink to="employers">For Employers</NavLink>
+        <NavLink to="/about">About Us</NavLink>
+        <NavLink to="contact">Contact Us</NavLink>
       </div>
-      <div>
-        <p>Log in</p>
-        <p>Register</p>
+      <div className="flex justify-between">
+        <button className="text-[#062182] border-2 border-[#062182] px-2 py-1">
+          Log in
+        </button>
+        <button className="bg-[#062182] text-white border-2 border-[#062182]">
+          Register
+        </button>
       </div>
     </main>
   );
